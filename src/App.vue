be2fed7ea17f162d699f0cf07ae5b6268f0f9b05
Ismoil4432 <ismoil4432@gmail.com>
2023-06-11 12:45:09 +0500

    enhance, explore sections

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
-  <div>
-    <h1>Hello World</h1>
-  </div>
+  <div><router-view></router-view></div>
 </template>
 
-<script setup></script>
+<script setup>
+
+</script>
 
 <style lang="scss" scoped></style>
---
+++ src/components/Container.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="container w-[1440px] mx-auto">
+    <slot />
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
